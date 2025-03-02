2ba7f815cfc2f33bcb417a0b7666008b6598a564
cekharchandra-devops <cekharchandra.devops@gmail.com>
2025-03-03 03:34:53 +0530

    consistent infra across dev and prod env poc using 3-tier application completed with tfvars

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output "public_ip" {
+  value = module.ec2_instance.public_ip
+}
+
+output "private_ip" {
+  value = module.ec2_instance.private_ip
+  
+}
+
+output "instance_id" {
+  value = module.ec2_instance.instance_id   
+  
+}
